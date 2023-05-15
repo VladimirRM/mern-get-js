df814809fc7a5870b296587f2403f0e17763f894
Vladimir RM <shvdrummer@gmail.com>
2023-05-15 21:06:20 +0300

    Work with router

---
+++ server/routes/auth.route.js
@@ -0,0 +1,15 @@
+const Router = require('express')
+
+
+const router = Router()
+
+router.post('/registration', async(req,res)=>{
+     try{
+
+     }catch(e){
+        console.log(e)
+     }
+})
+
+
+module.exports  = router
