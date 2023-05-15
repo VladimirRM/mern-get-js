495137c834d69fe7c5496dffcea02eb41660b732
Vladimir RM <shvdrummer@gmail.com>
2023-05-15 13:50:01 +0300

    Connect mongo

--- server/index.js
+++ server/index.js
@@ -4,3 +4,17 @@ const mongoose = require('mongoose')
 const app = express()
 
 const port = process.env.PORT || 5000 
+
+
+ async function start (){
+     try{
+      await mongoose.connect('mongodb+srv://user:user@cluster0.kpvcz4q.mongodb.net/?retryWrites=true&w=majority',{
+        useNewUrlParser : true,
+        useUnifyTopology:true,
+        useCreateIndex: true,
+        useFindAndModify:true
+      })
+     }catch(e){
+        console.log(e)
+     }
+}
