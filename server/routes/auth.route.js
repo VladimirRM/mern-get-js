82c71c41f8413d40778376f01a8bad8ff7aede83
Vladimir RM <shvdrummer@gmail.com>
2023-05-16 09:47:28 +0300

    Create candidate isUsed

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,11 +1,16 @@
 const Router = require('express')
+const User = require('../models/User')
 
 
 const router = Router()
 
 router.post('/registration', async(req,res)=>{
      try{
-
+     const {email,password} = req.body
+     const isUsed = await User.findOne({ email})
+     if(isUsed){
+        return res.status(300).json({message: 'Данный email уже существует'})
+     }
      }catch(e){
         console.log(e)
      }
